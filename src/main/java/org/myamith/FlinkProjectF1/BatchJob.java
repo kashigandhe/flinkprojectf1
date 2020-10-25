50ae376401ba4a1e9eaa5023433fdcde0c3a811b
manasa01 <manasa1bhat@gmail.com>
2020-10-24 21:27:40 -0500

    making wordcnt to batch

--- src/main/java/org/myamith/FlinkProjectF1/BatchJob.java
+++ src/main/java/org/myamith/FlinkProjectF1/BatchJob.java
@@ -18,7 +18,12 @@
 
 package org.myamith.FlinkProjectF1;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.aggregation.Aggregations;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
 
 /**
  * Skeleton for a Flink Batch Job.
@@ -60,7 +65,32 @@ public class BatchJob {
 		 *
 		 */
 
+		DataSet<String> sampleData = env.fromElements(
+				"Hello! This is a text sample",
+				"to represent Apache Flink streaming",
+				"Hello Hello Hello"
+		);
+
+		DataSet<Tuple2<String, Integer>> wordCountResults =
+				sampleData.flatMap(new LineSplitter())
+						.groupBy(0)
+						.aggregate(Aggregations.SUM, 1);
+
+		wordCountResults.print();
+
 		// execute program
 		env.execute("Flink Batch Java API Skeleton");
+	}
+
+	static class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+		@Override
+		public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+			String[] tokens = value.toLowerCase().split("\\W+");
+			for (String token : tokens) {
+				if (token.length() > 0) {
+					out.collect(new Tuple2<String, Integer>(token, 1));
+				}
+			}
+		}
 	}
 }
